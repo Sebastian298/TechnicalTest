0170695ed9eb0c927dc9c5019d4d45e18dea7165
Jaime Tenorio <jaimetenorio_geografia@outlook.es>
2023-05-02 21:22:54 -0500

    Creacion de un middleware para controlar las exepciones que puedan ocurrir durante algun proceso en la api, definiendo un formato adecuado para las respuestas.

--- TechnicalTest/Program.cs
+++ TechnicalTest/Program.cs
@@ -1,3 +1,5 @@
+using TechnicalTest.Middlewares;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -19,6 +21,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseMiddleware<CustomExceptionHandlerMiddleware>();
 
 app.MapControllers();
 
