15a56a0c44dbf38700e205393fe2adb8d7f68141
Jaime Tenorio <jaimetenorio_geografia@outlook.es>
2023-05-02 21:35:34 -0500

    Creacion de una clase para atrapar los errores que arrojan los data anotations de las propiedades de los modelos para poder darle un formato correcto

--- TechnicalTest/Program.cs
+++ TechnicalTest/Program.cs
@@ -1,3 +1,4 @@
+using TechnicalTest.Attributes;
 using TechnicalTest.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddControllers().ConfigureApiBehaviorOptions(options => { options.InvalidModelStateResponseFactory = ModelStateValidator.ValidModelState; }); ;
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
