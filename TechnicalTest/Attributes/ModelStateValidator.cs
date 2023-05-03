27ba99b105fb64e421171d4116bce05c14ef84b5
Jaime Tenorio <jaimetenorio_geografia@outlook.es>
2023-05-02 21:42:50 -0500

    Creacion de clase para atrapar los errores de formato de entrada en las propiedades de los modelos y regresar el error con un formato adecuado

---
+++ TechnicalTest/Attributes/ModelStateValidator.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using TechnicalTest.Models.Responses;
+
+namespace TechnicalTest.Attributes
+{
+    public class ModelStateValidator
+    {
+        public static IActionResult ValidModelState(ActionContext context)
+        {
+            var modelStateEntries = context.ModelState.Where(x => x.Value?.Errors.Count > 0);
+
+            var errorSerialized = string.Join(" | ", modelStateEntries.Select(x => x.Value?.Errors.FirstOrDefault()?.ErrorMessage));
+            dynamic response = new JObject();
+            response.message = "Verifique el formato de entrada";
+            response.exceptionMessage = errorSerialized;
+            var genericResponse = new GenericResponse<JObject>
+            {
+                StatusCode = 400,
+                Content = response
+            };
+
+            return new BadRequestObjectResult(genericResponse);
+        }
+    }
+}
