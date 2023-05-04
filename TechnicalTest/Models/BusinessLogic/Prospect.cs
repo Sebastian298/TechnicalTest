a1fff033531c3e83a4f0b452455eb2b105ec0275
Jaime Tenorio <jaimetenorio_geografia@outlook.es>
2023-05-03 22:46:42 -0500

    Creacion de repository y controller para crear los endpoints para la entidad Prospect

---
+++ TechnicalTest/Models/BusinessLogic/Prospect.cs
@@ -0,0 +1,9 @@
+﻿namespace TechnicalTest.Models.BusinessLogic
+{
+    public class Prospect
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ TechnicalTest/Repositories/ProspectRepository.cs
@@ -0,0 +1,44 @@
+﻿using Dapper;
+using System.Data;
+using TechnicalTest.Helpers;
+using TechnicalTest.Models.BusinessLogic;
+using TechnicalTest.Models.Responses;
+using TechnicalTest.Services;
+
+namespace TechnicalTest.Repositories
+{
+    public class ProspectRepository : IProspectRepository
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IDapperService _service;
+
+        public ProspectRepository(IConfiguration configuration,IDapperService dapperService)
+        {
+            _configuration = configuration;
+            _service = dapperService;
+        }
+        public async Task<GenericResponse<List<Prospect>>> GetAllProspectsAsync()
+        {
+            try
+            {
+                var spData = JsonReader.GetConfigurationStoredProcedure(_configuration, "StoredProceduresSettings:ProspectRepository:GetAll:Data");
+                var result = await _service.ExecuteStoredProcedureAsync<Prospect>(spData, null, true);
+                if (result.HasError)
+                {
+                    var error = MessageErrorBuilder.GenerateError(result.Message);
+                    return new GenericResponse<List<Prospect>>() { StatusCode = 500, Message = error };
+                }
+                return new GenericResponse<List<Prospect>>()
+                {
+                    StatusCode = 200,
+                    Content = result.Results
+                };
+            }
+            catch (Exception ex)
+            {
+                var message = MessageErrorBuilder.GenerateError(ex.Message);
+                return new GenericResponse<List<Prospect>>() { StatusCode = 500, Message = message };
+            }
+        }
+    }
+}
